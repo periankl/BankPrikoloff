d282a02fb04cfb5ba4596b25612efd34cd3ded38
MeidKoi <dshinotanrid@gmail.com>
2024-11-18 15:08:26 +0300

    Add Email Send

---
+++ BankPrikoloff/Controllers/EmailController.cs
@@ -0,0 +1,47 @@
+using System.Net;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Mvc;
+using BankPrikoloff.Contracts;
+using BusinessLogic.Interfaces;
+using BusinessLogic.Servises;
+using Domain.Models;
+using Mapster;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BankPrikoloff.Controllers
+{
+[ApiController]
+[Route("api/[controller]")]
+public class EmailController : ControllerBase
+{
+    [HttpPost("send")]
+    public IActionResult SendEmail([FromBody] EmailRequest request)
+    {
+            
+            MailAddress from = new MailAddress("support@bank-prikoloff.ru");
+            MailAddress to = new MailAddress(request.To);
+            MailMessage message = new MailMessage(from, to);
+            message.Subject = request.Subject;
+            message.Body = request.Body;
+
+            SmtpClient mailClient = new SmtpClient("smtp.yandex.ru");
+            mailClient.Credentials = new NetworkCredential("support@bank-prikoloff.ru", "kcimpuflixjznfjg");
+            mailClient.Port = 587;
+            mailClient.EnableSsl = true;
+            mailClient.UseDefaultCredentials = false;
+            mailClient.Send(message);
+
+            return Ok("Email sent successfully.");
+    }
+}
+
+public class EmailRequest
+{
+    public string To { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+}
+
+}
