dde6f925f4bdfd05b2ec13797fa16fde20eee137
MeidKoi <dshinotanrid@gmail.com>
2024-11-18 15:11:16 +0300

    Zzzabil

--- BankPrikoloff/Controllers/EmailController.cs
+++ BankPrikoloff/Controllers/EmailController.cs
@@ -1,6 +1,3 @@
-using System.Net;
-using System.Net.Mail;
-using Microsoft.AspNetCore.Mvc;
 using BankPrikoloff.Contracts;
 using BusinessLogic.Interfaces;
 using BusinessLogic.Servises;
@@ -8,18 +5,21 @@ using Domain.Models;
 using Mapster;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
 
 
 namespace BankPrikoloff.Controllers
 {
-[ApiController]
-[Route("api/[controller]")]
-public class EmailController : ControllerBase
-{
-    [HttpPost("send")]
-    public IActionResult SendEmail([FromBody] EmailRequest request)
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmailController : ControllerBase
     {
-            
+        [HttpPost("send")]
+        public IActionResult SendEmail([FromBody] EmailRequest request)
+        {
+
             MailAddress from = new MailAddress("support@bank-prikoloff.ru");
             MailAddress to = new MailAddress(request.To);
             MailMessage message = new MailMessage(from, to);
@@ -34,14 +34,14 @@ public class EmailController : ControllerBase
             mailClient.Send(message);
 
             return Ok("Email sent successfully.");
+        }
     }
-}
 
-public class EmailRequest
-{
-    public string To { get; set; }
-    public string Subject { get; set; }
-    public string Body { get; set; }
-}
+    public class EmailRequest
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
 
 }
