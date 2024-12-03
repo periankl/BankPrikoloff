207a55e0354e8f77cf2fa26333760f1e0bf3fc34
periankl <periankl1@gmail.com>
2024-12-03 11:26:27 +0300

    migrations update

---
+++ Domain/Migrations/20241203082332_ModifyUser.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    public partial class ModifyUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "Relationship4",
+                table: "Card");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Relationship4",
+                table: "Card");
+
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "Card");
+
+            migrationBuilder.DropColumn(
+                name: "CurrencyID",
+                table: "Card");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Patronomic",
+                table: "Users",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldUnicode: false,
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldUnicode: false,
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldUnicode: false,
+                oldMaxLength: 50);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AcceptTerms",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PasswordReset",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ResetToken",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ResetTokenExpires",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Updated",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "VerificationToken",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Verified",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Message",
+                type: "nvarchar(1024)",
+                maxLength: 1024,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(1024)",
+                oldUnicode: false,
+                oldMaxLength: 1024);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AccountStatus",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldUnicode: false,
+                oldMaxLength: 50);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AccountNumber",
+                table: "Account",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.CreateTable(
+                name: "RefreshToken",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountClientId = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Token = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Expires = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedByIp = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Revoked = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    RevokedByIp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReplacedByToken = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReasonRevoked = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RefreshToken", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RefreshToken_Users_AccountClientId",
+                        column: x => x.AccountClientId,
+                        principalTable: "Users",
+                        principalColumn: "ClientID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshToken_AccountClientId",
+                table: "RefreshToken",
+                column: "AccountClientId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RefreshToken");
+
+            migrationBuilder.DropColumn(
+                name: "AcceptTerms",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordReset",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ResetToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ResetTokenExpires",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Updated",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "VerificationToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Verified",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "AccountNumber",
+                table: "Account");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Patronomic",
+                table: "Users",
+                type: "varchar(50)",
+                unicode: false,
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "varchar(50)",
+                unicode: false,
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "varchar(50)",
+                unicode: false,
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Message",
+                type: "varchar(1024)",
+                unicode: false,
+                maxLength: 1024,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1024)",
+                oldMaxLength: 1024);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Balance",
+                table: "Card",
+                type: "decimal(10,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CurrencyID",
+                table: "Card",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AccountStatus",
+                type: "varchar(50)",
+                unicode: false,
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Relationship4",
+                table: "Card",
+                column: "CurrencyID");
+
+            migrationBuilder.AddForeignKey(
+                name: "Relationship4",
+                table: "Card",
+                column: "CurrencyID",
+                principalTable: "Currency",
+                principalColumn: "CurrencyID");
+        }
+    }
+}
--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -30,7 +30,23 @@ namespace Domain.Models
         public int? DeletedBy { get; set; }
         public DateTime? DeletedAt { get; set; }
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        
+        public DateTime Created {  get; set; }
+        public DateTime Updated { get; set; } 
+        public bool AcceptTerms { get; set; }
+        public string? VerificationToken { get; set; }
 
+        public DateTime? Verified { get; set; }
+        public bool IsVerified => Verified.HasValue || PasswordReset.HasValue;
+        public string? ResetToken { get; set; }
+        public DateTime? ResetTokenExpires { get; set; }
+
+        public DateTime? PasswordReset { get; set; }
+        public List<RefreshToken> RefreshTokens { get; set; }
+        public bool OwnsToken(string token)
+        {
+            return this.RefreshTokens?.Find(x => x.Token == token) != null;
+        }
         public virtual Chat? Chat { get; set; } = null!;
         public virtual Role? Role { get; set; } = null!;
         public virtual ICollection<Account>? Accounts { get; set; }
